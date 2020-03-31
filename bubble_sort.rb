ff170248e15d4275eea1ebf4511ac60ef12bf6c5
phillip musiime <phillipmusiime@gmail.com>
2020-03-31 18:36:25 +0300

    completed first #bubble_sort method. Works fine

---
+++ bubble_sort.rb
@@ -0,0 +1,13 @@
+def bubble_sort(arr)
+  i = arr.length
+  (0..i).each do |k|
+    ((k + 1)..i - 1).each do |j|
+      next unless arr[k] > arr[j]
+
+      placeholder = arr[j]
+      arr[j] = arr[k]
+      arr[k] = placeholder
+    end
+  end
+  p arr
+end
