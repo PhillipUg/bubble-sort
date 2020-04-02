897207d0d9fd81bc9c437706092bf88d9533b6b7
phillip musiime <phillipmusiime@gmail.com>
2020-04-02 09:52:59 +0300

    Modified the bubble_sort method to accept blocks using yeild

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -9,5 +9,19 @@ def bubble_sort(arr)
       arr[k] = placeholder
     end
   end
-  p arr
+  arr
+end
+
+def bubble_sort_by(arr)
+  i = arr.length
+  (0..i).each do |k|
+    ((k + 1)..i - 1).each do |j|
+      next unless yield(arr[k], arr[j]).positive?
+
+      placeholder = arr[j]
+      arr[j] = arr[k]
+      arr[k] = placeholder
+    end
+  end
+  arr
 end
